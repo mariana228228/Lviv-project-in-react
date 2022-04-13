cd186a98406462e9730665efd520e0bac88c59a6
mariana228228 <maryanabatig123@gmail.com>
2022-04-13 13:29:26 +0300

    first commit

---
+++ src/Header/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import photo from './XIX.jpg'
+
+function Header() {
+  return (
+    <div>
+   <h1>Історія Львова</h1>
+   <img src={photo} alt="" />
+   <hr />
+    </div>
+  );
+}
+
+export default Header;
